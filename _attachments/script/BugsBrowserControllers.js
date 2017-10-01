0b8b21a0568194de6a68fa6c63b920e0560edbf9
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-10-01 06:51:01 -0400

    Remove animations

--- _attachments/script/BugsBrowserControllers.js
+++ _attachments/script/BugsBrowserControllers.js
@@ -34,69 +34,6 @@
         $scope.endNumber = $scope.paginator.pageSize;
         $scope.loading = true;
 
-        // List animations
-        $scope.animationFade = {
-            enter: 'fade-enter',
-            leave: 'fade-leave'
-        };
-        $scope.animationNextPage = {
-            enter: 'nextpage-enter',
-            leave: 'nextpage-leave'
-        };
-        $scope.animationPreviousPage = {
-            enter: 'previouspage-enter',
-            leave: 'previouspage-leave'
-        };
-        $scope.listAnimation = $scope.animationFade;
-
-        $scope.setNextPageAnimation = function() {
-            $scope.listAnimation = $scope.animationNextPage;
-        };
-
-        $scope.setPreviousPageAnimation = function() {
-            $scope.listAnimation = $scope.animationPreviousPage;
-        };
-
-        $scope.getListAnimation = function() {
-            return $scope.listAnimation;
-        };
-
-        /**
-         * Takes two lists of bugs and merges the second one into the first one.
-         * New bugs are added, bugs absent from list2 are removed from list1, and updated bugs are examined to update
-         * the values from list1 with those from list2.
-         * This is necessary to allow to animate only changes.
-         * @param list1 Original list.
-         * @param list2 New list.
-         */
-        var mergeBugsLists = function(list1, list2) {
-            var bugslist = {};
-            for(var i1 = 0; i1 < list1.length; i1++) {
-                bugslist[list1[i1].id] = {idxlist1: i1};
-            }
-            for(var i2 = 0; i2 < list2.length; i2++) {
-                if(!bugslist[list2[i2].id]){
-                    // Mark bug as not found in list1
-                    bugslist[list2[i2].id] = {idxlist1: -1};
-                }
-                bugslist[list2[i2].id].idxlist2 = i2;
-            }
-            for(var iBugs in bugslist) {
-                if(bugslist[iBugs].idxlist1 < 0 && bugslist[iBugs].idxlist2 >= 0) {
-                    // New bug
-                    list1.push(list2[bugslist[iBugs].idxlist2]);
-                } else if (bugslist[iBugs].idxlist1 >= 0 && bugslist[iBugs].idxlist2 < 0) {
-                    // Deleted bug
-                    list1.splice(bugslist[iBugs].idxlist1, 1);
-                } else if(bugslist[iBugs].idxlist1 >= 0 && bugslist[iBugs].idxlist2 >= 0) {
-                    if(!list1[bugslist[iBugs].idxlist1].equals(list2[bugslist[iBugs].idxlist2])) {
-                        // Updated bug
-                        list1[bugslist[iBugs].idxlist1].updateWithBug(list2[bugslist[iBugs].idxlist2]);
-                    }
-                }
-            }
-        };
-
         $scope.incPage = function() {
             $scope.previousStartKeys.push($scope.startKey);
             $scope.startKey = $scope.nextKey;
@@ -140,8 +77,6 @@
                 $scope.startNumber = ($scope.previousStartKeys.length * $scope.paginator.pageSize) + 1;
                 $scope.endNumber = $scope.startNumber + $scope.bugs.length - 1;
 
-                $scope.listAnimation = $scope.animationFade;
-                mergeBugsLists($scope.bugs, data.rows);
                 for(var row = 0; row < $scope.bugs.length; row++) {
                     $scope.bugs[row].latest = moment($scope.bugs[row].value.latest).fromNow();
                 }
